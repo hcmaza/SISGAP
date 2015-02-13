928fdd9d8ad1ecb95b7f47180526037773ab1fe4
Martin Paez Yañez <martinpy@gmail.com>
2015-02-13 20:49:06 -0300

    #Cambios menores en las vistas de desembolsos, solicitudes, solicitudes acta y rendiciones. [Solicitudacta]: se hicieron correcciones en un metodo de consulta del facade.

--- src/main/java/ar/edu/undec/sisgap/controller/SolicitudactaFacade.java
+++ src/main/java/ar/edu/undec/sisgap/controller/SolicitudactaFacade.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ar.edu.undec.sisgap.controller;
 
 import ar.edu.undec.sisgap.model.Solicitudacta;
@@ -20,6 +19,7 @@ import javax.persistence.Query;
  */
 @Stateless
 public class SolicitudactaFacade extends AbstractFacade<Solicitudacta> {
+
     @PersistenceContext(unitName = "ar.edu.undec_SYSGAP_war_1.0-SNAPSHOTPU")
     private EntityManager em;
 
@@ -38,22 +38,20 @@ public class SolicitudactaFacade extends AbstractFacade<Solicitudacta> {
 //        from Person q
 //        group by q.email having count(q.email)>1)
 //    order by p.email, p.id
-
     
-    public List<Solicitudacta> obtenerPorProyecto(int proyectoid){
+    public List<Solicitudacta> obtenerPorProyecto(int proyectoid) {
         //Query consulta = em.createQuery("SELECT sa FROM Solicitudacta sa WHERE sa.listaSolicitudes.presupuestotarea.tarea.etapaid.proyectoid.id = :proyectoid", Solicitudacta.class);
-        
-        try{
-            Query consulta = em.createQuery("SELECT sa FROM Solicitudacta sa WHERE sa.id IN (SELECT s FROM Solicitud s WHERE s.presupuestotarea.tarea.etapaid.proyectoid.id = :proyectoid)", Solicitudacta.class);
-        consulta.setParameter("proyectoid", proyectoid);
-        return consulta.getResultList();
-        } catch(Exception e){
+
+        try {
+            Query consulta = em.createQuery("SELECT sa FROM Solicitudacta sa WHERE sa.id IN (SELECT s.solicitudactaid.id FROM Solicitud s WHERE s.presupuestotarea.tarea.etapaid.proyectoid.id = :proyectoid)", Solicitudacta.class);
+            consulta.setParameter("proyectoid", proyectoid);
+            return consulta.getResultList();
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("SolicitudactaFacade - obtenerPorProyecto");
             return new ArrayList<Solicitudacta>();
         }
-        
-        
+
     }
-    
+
 }
